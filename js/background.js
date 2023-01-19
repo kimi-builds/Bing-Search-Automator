0033316de2a9b2ed27a59afa4f662e06915fdeb7
kimi-builds <90973263+kimi-builds@users.noreply.github.com>
2023-01-19 13:10:54 -0300

    Update to v 20230119

---
+++ js/background.js
@@ -0,0 +1,27 @@
+chrome.action.onClicked.addListener(function (tab) {
+  chrome.tabs.create({ url: '../index.html', active: true });
+  })
+
+async function getTabId() {
+    return new Promise( (resolve, reject) => {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            var activeTab = tabs[0]
+            var activeTabId = activeTab.id
+            resolve(activeTabId)
+        })
+    })
+}
+
+// Close debugger in case is open when close popup
+chrome.runtime.onConnect.addListener(async function(port) {
+    if (port.name === "popup") {
+        port.onDisconnect.addListener(async function() {
+           
+           let tabId = await getTabId()
+
+           chrome.debugger.detach({ tabId }, function () {
+                console.log(`Debugger disables by tab: ${tabId}`);
+            })
+        })
+    }
+})
